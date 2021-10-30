b7d544977dab03a51c0a80c8120be391e5f4a310
Tomasbidegain <tomasbidegain8888@gmail.com>
2021-10-30 14:55:06 -0300

    corrigiendo errores

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import AppClima from './AppClima';
+import './index.css';
 
 ReactDOM.render(
     <AppClima />,
